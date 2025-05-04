c5828f872a755c6fc95f0822c6f897c9178082af
SantunesIgor <igorsantunes@outlook.com>
2025-05-04 19:33:05 -0300

    04052025

--- 7. Flappy Bird HTML/assets/js/script.js
+++ 7. Flappy Bird HTML/assets/js/script.js
@@ -1,1 +1,35 @@
-document.gerElementbyId
+const yellowbird = document.getElementById("yellowbird");
+const frame = document.querySelector('.frame');
+
+let y = yellowbird.offsetTop;
+let velocity = 0;
+const gravity = 0.1;
+const jumpStrength = -4;
+
+function updatePosition() {
+  velocity += gravity;
+  y += velocity;
+
+  const maxY = frame.clientHeight + 100;
+  if (y > maxY) {
+    y = maxY;
+    velocity = 0;
+  }
+
+  if (y < 140) {
+    y = 140;
+    velocity = 0;
+  }
+
+  yellowbird.style.top = `${y}px`;
+
+  requestAnimationFrame(updatePosition);
+}
+
+document.addEventListener('keydown', function (event) {
+  if (event.key === "ArrowUp") {
+    velocity = jumpStrength;
+  }
+});
+
+updatePosition();
